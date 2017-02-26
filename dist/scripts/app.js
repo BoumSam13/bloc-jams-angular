ca1705aaecc239448aa5ef1f47d75b065b7214f1
Samantha Boumeester <sam_boumeester41@hotmail.com>
2017-02-25 19:51:13 -0600

    Assignment 3 routingStates: completed

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -2,19 +2,23 @@
      function config($stateProvider, $locationProvider) {
          $locationProvider
          .html5Mode({
-             enabled: true,
-             requireBase: false
+            enabled: true,
+            requireBase: false
          });
 
      $stateProvider
          .state('landing', {
-             url: '/',
-             templateUrl: '/templates/landing.html'
+            url: '/',
+            templateUrl: '/templates/landing.html'
          })
          .state('album', {
-             url: '/album',
-             templateUrl: '/templates/album.html'
-         });
+            url: '/album',
+            templateUrl: '/templates/album.html'
+         })
+        .state('collection', {
+            url: '/collection',
+            templateUrl: '/templates/collection.html'
+        });
      }
  
      angular
